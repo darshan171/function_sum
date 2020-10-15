851f2ca2ba5f36e23f1b73a997e9f51d00515d0c
unknown <patel.darshan36@gmail.com>
2020-10-15 11:24:00 -0600

    version 1.0

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine ("Please enter the smaller number A = ");
+            string userinputA = Console.ReadLine();
+            int a = Convert.ToInt32(userinputA);
+
+            Console.WriteLine ("Please enter the larger number B = ");
+            string userinputB = Console.ReadLine();
+            int b = Convert.ToInt32(userinputB);
+
+            if (a > b)
+            {
+                Console.WriteLine(" Error !!! A must be smaller then B");
+                return;
+            }
+            // calling function 
+            int sum = sumbetweennumbers(a,b);
+            Console.WriteLine("= " + sum);
+            
+        
+        }
+            //fuction code
+            static int sumbetweennumbers(int a, int b)
+        {
+            int sum = 0;
+           
+            for (int i = a; i <= b; i++)
+            {
+                sum += i;
+                Console.WriteLine("+" + i);
+            }
+
+            Console.WriteLine("---------------------------");
+            //Console.WriteLine("=" + sum);
+            return sum;
+
+        }
+        
+    }
+}
